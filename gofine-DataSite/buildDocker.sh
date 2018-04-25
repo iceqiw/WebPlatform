f06939348099c54d2248821df16dcb3f16c435ce
iceqiw <qqwei1123@163.com>
2018-04-25 13:57:02 +0800

    docker

--- gofine-DataSite/buildDocker.sh
+++ gofine-DataSite/buildDocker.sh
@@ -3,6 +3,6 @@ echo "----------------mvn-------install---------"
 mvn clean install -Ptest
 
 echo "------------docker-----------install---------"
-docker build -t cloud/datasite -q .
+docker build -t cloud/datasite -q server/
 
 echo "------------docker-----------ok---------"
